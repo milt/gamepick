e3a547b78fe9a3fa04bb620022fdfad65b4dfaa4
Milton Reder <milt@jhu.edu>
2012-10-24 15:10:30 -0400

    implemented config

--- gamepick.rb
+++ gamepick.rb
@@ -1,15 +1,27 @@
-# demo.rb
+# gamepick.rb
 
 require 'rubygame'
+require 'yaml'
 include Rubygame
 
-res = Screen.get_resolution()
+config = YAML.load_file 'config/config.yml'
+
+rompath = config["paths"]["rom"]
+mamepath = config["paths"]["mame"]
+fontpath = config["paths"]["font"]
+maxres = config["display"]["maxres"]
+
+
+dev_res = Screen.get_resolution()
 target = [1024,768]
 
-rompath = "/home/pi/.advance/rom"
-romdir = Dir.new(rompath)
+if (dev_res[0] > maxres[0]) || (dev_res[1] > maxres[1])
+  res = maxres
+else
+  res = dev_res
+end
 
-mamepath = "/home/pi/test/mame/mameBin/bin/advmame"
+romdir = Dir.new(rompath)
 
 games = []
 romdir.each {|filename| games << filename}
@@ -25,14 +37,13 @@ Rubygame.init
 TTF.setup
 
 def title(string,font,screen,coords)
-  #puts "No antialiasing, no background..."
   text = font.render(">" + string,false,[0,255,0])
   text.blit(screen,coords)
 end
 
-font = TTF.new("8bit.ttf",200)
+font = TTF.new(fontpath,200)
 
-screen = Screen.new(res) #,0,[HWSURFACE])
+screen = Screen.new(res)
 screen.title = 'Hello World'
 screen.fill [0,0,0]
 title(games.first,font,screen,texpos)
@@ -50,12 +61,7 @@ until game_over do
   queue.each do |event|
     case event
       when ActiveEvent
-        #title(games.first,font,screen,texpos)
         screen.update
-#      when Events::WindowExposed
-#        screen.fill [0,0,0]
-#        title(games.first,font,screen,texpos)
-#        screen.update
       when QuitEvent
         game_over = true
       when KeyDownEvent
